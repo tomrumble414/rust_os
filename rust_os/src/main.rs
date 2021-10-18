e8f87212a512ff2f287ac0ac624b545e87fe633a
tom-rumble <tomrumble414@gmail.com>
2021-10-18 14:02:39 +1100

    buildable kernel

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
