b92bfb9b6a327ec2dc455a2d89b856051c02b9d3
tom-rumble <tomrumble414@gmail.com>
2021-10-18 21:46:09 +1100

    vga buffer
    basic implementation to display text using the vga buffer

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -1,9 +1,12 @@
 #![no_std]
 #![no_main]
 
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
     loop {}
 }
 
@@ -14,3 +17,5 @@ use core::panic::PanicInfo;
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
+mod vga_buffer;
